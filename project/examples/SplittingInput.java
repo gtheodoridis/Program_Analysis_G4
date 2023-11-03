fdfd05f6510d81e7cc460077437e05effefb6c4d
gtheodoridis <giorgostheo21@gmail.com>
2023-11-03 15:41:34 +0100

    Splitting Input

---
+++ project/examples/SplittingInput.java
@@ -0,0 +1,12 @@
+public class SplittingInput {
+    public static void main(String[] args) {
+        
+        String input = args[0];
+        
+        // Split the input into two halves
+        String new_var = input.substring(0, input.length() / 2);
+        
+        // Attempting to access an index outside the bounds of the new_var
+        char invalidAccess = new_var.charAt(input.length() - 1);
+    }
+}
