56ad00c65fc7d349815275765985dd81ca2af7bb
uu <gerda.63rd4@gmail.com>
2023-11-10 14:15:44 +0100

    arrays work

--- project/src/TaggedInterpreter.py
+++ project/src/TaggedInterpreter.py
@@ -46,6 +46,11 @@ class TaggedInterpreter(BaseInterpreter):
         for ind in range(len(os)):
             os[ind] = self._class(os[ind], ["SV{}".format(ind)])
 
+        for val in range(len(self.memory)):
+            for ind in range(len(self.memory[val])):
+                self.memory[val][ind] = self._class(self.memory[val][ind], ["MVEL{}".format(ind)])
+            self.memory[val] = TaggedValue(self.memory[val], ["MV{}".format(val)])
+
         ret = super().run(f)
 
         logger.info(self.history)
@@ -159,11 +164,11 @@ class TaggedInterpreter(BaseInterpreter):
 
         # index_array is assumed to not be a range but an integer
         index_array = os[-2]  # Index of the array
-        if index_el.start < 0 or index_el.start >= len(self.memory[index_array]):
+        if self._class._get(index_el) < 0 or self._class._get(index_el) >= len(self._class._get(self.memory[self._class._get(index_array)])):
             raise Exception("IndexOutOfBoundsException")
-        if index_array < 0 or index_array >= len(self.memory):
+        if self._class._get(index_array) < 0 or self._class._get(index_array) >= len(self.memory):
             raise Exception("NullPointerException")
-        value = self.memory[index_array][index_el.start]  # Get the value from memory
+        value = self._class._get(self.memory[self._class._get(index_array)])[self._class._get(index_el)]  # Get the value from memory
         self.stack.append((lv, os[:-2] + [value], pc + 1))
 
     def _array_store(self, b):
@@ -172,41 +177,35 @@ class TaggedInterpreter(BaseInterpreter):
         self.history["last_opr"] = {"opr_name":"_array_store", "args":os[-1]}
         value = os[-1]  # Value to store
         # index_array is assumed to not be a range but an integer
-        index_of_array = os[-3]  # Index of the array to store in
-        index_of_el = os[-2]  # Index of the element in the array
-
-        # TODO: maybe replace with for loop        
-        assert index_of_el.start == index_of_el.end
+        index_of_array = self._class._get(os[-3])  # Index of the array to store in
+        index_of_el = self._class._get(os[-2])  # Index of the element in the array
         
-        if index_of_el.start < 0 or index_of_el.start >= len(self.memory[index_of_array]):
+        if index_of_el < 0 or index_of_el >= len(self._class._get(self.memory[index_of_array])):
             raise Exception("IndexOutOfBoundsException")
         if index_of_array < 0 or index_of_array >= len(self.memory):
             raise Exception("NullPointerException")
-        self.memory[index_of_array][index_of_el.start] = value  # Otherwise, update the existing element
+        self._class._get(self.memory[index_of_array])[index_of_el] = value  # Otherwise, update the existing element
         self.stack.append((lv, os[:-3], pc + 1))
 
     def _newarray(self, b):
         # Create a new array and store its index
         (lv, os, pc) = self.stack.pop(-1)
         self.history["last_opr"] = {"opr_name":"_arraylength", "args":os[-1]}
-        size = os[-1]
-
-        # TODO: maybe replace with for loop        
-        assert size.start == size.end
+        size = self._class._get(os[-1])
 
         # TODO: use dimension
         dim = b["dim"]
-        value = [None] * size.start
         tag = "ARR{}".format(self.new_arr_counter)
+        value = [TaggedValue(None, ["ARREL{}".format(i)]) for i in range(size)]
         self.new_arr_counter += 1
         self.memory.append(TaggedValue(value, [tag]))  # Create a new empty array in memory
-        self.stack.append((lv, os + [len(self.memory)-1], pc + 1))  # Store the index of the new array
+        self.stack.append((lv, os + [TaggedValue(len(self.memory)-1, [tag])], pc + 1))  # Store the index of the new array
 
     def _arraylength(self, b):
         # Get the length of an array
         (lv, os, pc) = self.stack.pop(-1)
         self.history["last_opr"] = {"opr_name":"_arraylength", "args":[os[-1], self.memory[index_array]]}
-        index_array = os[-1]  # Index of the array
+        index_array = self._class._get(os[-1])  # Index of the array
         value = len(self.memory[index_array].value)  # Get the length of the array in memory
         print("arraylength", value)
         print("arraylength", self.memory[index_array].tags)
